bf06dff9cd36c3ae530fb7df6eb1d044d655856a
Dylan Tastet <dylanjtastet@gmail.com>
2019-01-31 19:36:10 -0500

    initial commit

---
+++ ex4.c
@@ -0,0 +1,14 @@
+/* Example: matrices represented by 2-dimensional arrays */
+
+#include <stdio.h>
+
+int main()
+{
+  int A[3][3];    // matrix A     
+  int B[3][3];    // matrix B
+  int C[3][3];    // matrix to store their sum
+
+  // add your code below
+  
+
+}
